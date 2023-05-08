f8c89fc95055059c496e81a4c236ed75db2fe5c8
Luiz Antonio Dos Santos <102430261+luiz01204@users.noreply.github.com>
2023-05-08 08:32:29 -0300

    v_1

---
+++ src/main/java/br/com/alphaclean/alphaSystem/AlphaSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.alphaclean.alphaSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaSystemApplication.class, args);
+	}
+
+}
